dcc26b55ec3a4d7ae40643d4794e211f9c75d773
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2023-04-10 11:36:21 +0200

    Add ag-grid table

---
+++ content/docs/ag-grid.md
@@ -0,0 +1,18 @@
+---
+title: "Ag-grid"
+weight: 1
+# bookFlatSection: false
+# bookToc: true
+# bookHidden: false
+# bookCollapseSection: false
+# bookComments: false
+# bookSearchExclude: false
+---
+
+# Test Ag-grid
+
+### shortcode `agg` 
+
+{{< agg >}}
+
+
---
+++ static/js/agg.js
@@ -0,0 +1,25 @@
+const columnDefs = [
+  { field: "make" },
+  { field: "model" },
+  { field: "price" }
+];
+
+// specify the data
+const rowData = [
+  { make: "Toyota", model: "Celica", price: 35000 },
+  { make: "Ford", model: "Mondeo", price: 32000 },
+  { make: "Porsche", model: "Boxster", price: 72000 }
+];
+
+// let the grid know which columns and what data to use
+const gridOptions = {
+  columnDefs: columnDefs,
+  rowData: rowData
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+  console.log ("=== init ===")
+    const gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+});

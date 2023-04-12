00f5d19443599c9053dc7de8388182e2c650717f
Julie921 <halbout.julie@gmail.com>
2023-04-12 16:28:53 +0200

    (tools) : modification du script build_ag-grid.py pour passer les arguments request_file et corpora_file  en ligne de commande

--- tools/build_ag-grid_table.py
+++ tools/build_ag-grid_table.py
@@ -1,43 +1,55 @@
+"""
+Pour lancer le script : 
+
+python3 build_ag-grid_table.py --request_file chemin/vers/requetes/json --corpora_file chemin/vers/json/corpus
+"""
 import datetime
+import argparse
 import sys
 import json
 from grewpy import Request, Corpus, set_config
 
 set_config('sud')
 
-request_file = "subj_requests.json"
-corpora_file = "sud_french.json"
-
-
-with open(request_file, "rb") as f:
-  grew_requests = { x["id"]: Request.from_json(x["request"]) for x in json.load(f) }
-
-with open(corpora_file, "rb") as f:
-  json_data = json.load(f)
-  corpora = { x["id"]: x["directory"] for x in json_data["corpora"] }
-
 if __name__ == '__main__':
-  main_dict = {}
-  for corpus_name in corpora:
-    corpus = Corpus(corpora[corpus_name])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--request_file', help='Path to the JSON file containing the requests')
+    parser.add_argument('--corpora_file', help='Path to the JSON file containing the corpora')
+    args = parser.parse_args()
 
-    main_dict[corpus_name] = { id: corpus.count(grew_requests[id]) for id in grew_requests }
-    corpus.clean()
+    request_file = args.request_file
+    corpora_file = args.corpora_file
 
-  columnDefs = [ { "field": "row_header", "headerName": "Treebank", "pinned": "left", "lockPinned": True} ] 
-  columnDefs += [ {"field": id, "headerName": id, "request": str(grew_requests[id])} for id in grew_requests ]
 
-  rowData = [ {"row_header": k1 } | { k2: main_dict[k1][k2] for k2 in main_dict[k1]} for k1 in main_dict]
+    with open(request_file, "rb") as f:
+      grew_requests = { x["id"]: Request.from_json(x["request"]) for x in json.load(f) }
 
-  data = { 
-    "defaultColDef": {
-      "width": 150,
-      "sortable": True,
-      "sortingOrder": ["desc", "asc"],
-      "resizable": True
-    },
-    "columnDefs": columnDefs,
-    "rowData": rowData,
-  }
+    with open(corpora_file, "rb") as f:
+      json_data = json.load(f)
+      corpora = { x["id"]: x["directory"] for x in json_data["corpora"] }
 
-  print (json.dumps(data, indent=2))
+    if __name__ == '__main__':
+      main_dict = {}
+      for corpus_name in corpora:
+        corpus = Corpus(corpora[corpus_name])
+
+        main_dict[corpus_name] = { id: corpus.count(grew_requests[id]) for id in grew_requests }
+        corpus.clean()
+
+      columnDefs = [ { "field": "row_header", "headerName": "Treebank", "pinned": "left", "lockPinned": True} ] 
+      columnDefs += [ {"field": id, "headerName": id, "request": str(grew_requests[id])} for id in grew_requests ]
+
+      rowData = [ {"row_header": k1 } | { k2: main_dict[k1][k2] for k2 in main_dict[k1]} for k1 in main_dict]
+
+      data = { 
+        "defaultColDef": {
+          "width": 150,
+          "sortable": True,
+          "sortingOrder": ["desc", "asc"],
+          "resizable": True
+        },
+        "columnDefs": columnDefs,
+        "rowData": rowData,
+      }
+
+      print (json.dumps(data, indent=2))

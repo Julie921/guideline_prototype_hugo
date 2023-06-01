4d50b96c348b1e0c9df364743e611203b27652fe
Julie921 <halbout.julie@gmail.com>
2023-06-01 13:15:52 +0200

    (tools) : changement dans le script new_language pour améliorer l'environnement de travail (ajout des sous-titres dans toutes les pages, création d'un dossier de la langue dans le guide, création d'une page index à remplir, ajout des titres sur toutes les pages

--- tools/new_language.sh
+++ tools/new_language.sh
@@ -80,4 +80,35 @@ echo $json_content
 # Affiche le chemin du fichier JSON généré
 echo "Le fichier JSON a été généré : $json_file"
 
+
+
+# Chemin de la racine de l'arborescence
+racine="../content/docs/general_guideline"
+
+# Vérifier si le répertoire racine existe
+if [ ! -d "$racine" ]; then
+  echo "Le répertoire racine $racine n'existe pas."
+  exit 1
+fi
+
+# récupérer le nombre de fichier 
+nombre_fichiers = 0
+# Itérer sur tous les fichiers de l'arborescence
+find "$racine" -type f | while read -r fichier; do
+  nom_fichier=$(basename "$fichier")
+    # Vérifier si le fichier ne commence pas par "_"
+  if [[ ! $nom_fichier =~ ^_ ]]; then
+    echo "$nom_fichier"
+    echo -e "## $folder_name \n" >> "$nom_fichier"
+    nombre_fichiers=$((nombre_fichiers + 1))
+  fi
+done
+
+# Créer le sous-dossier dans le guide d'annotation pour la langue
+
+mkdir ../content/docs/language/"$folder_name"
+touch ../content/docs/language/"$folder_name"/_index.md
+echo -e "# $folder_name \n ## General information \n ## Treebank information \n There is $nombre_fichiers pages to fill. \n Statut of the guideline : 0% written \n ## Author information \n" > ../content/docs/language/"$folder_name"/_index.md
+
+
 exit 0

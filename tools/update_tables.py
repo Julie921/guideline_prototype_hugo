241235cdb7db6fc0739ccc53f6a542dc8417c634
Julie921 <halbout.julie@gmail.com>
2023-06-15 15:02:20 +0200

    ajout script python et .yml pour intégration continue

--- tools/update_tables.py
+++ tools/update_tables.py
@@ -5,6 +5,8 @@ Script python utiliser pour mettre à jour toutes les tables à chaque push
 
 import os 
 from test_build_table import process_files
+import shutil
+
 
 def get_files_in_directory(directory):
     files = []
@@ -29,13 +31,21 @@ def get_subdirectories_paths(directory):
             subdirectories_paths.append((entry.path))
     return subdirectories_paths
 
-if __name__ == "__main__":
-    # # Exemple d'utilisation
-    # directory_path = "/chemin/vers/votre/dossier"
-    # files_list = get_files_in_directory(directory_path)
-    # print(files_list)
+def find_file_by_name(directory, filename):
+    for root, dirs, files in os.walk(directory):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
 
-    # Exemple d'utilisation
+
+def replace_file(directory,filename,actual_table_path):
+    file_path = find_file_by_name(directory, filename)
+    file_path_r = str(file_path).split("/")
+    new_file_path = f"{'/'.join(file_path_r[:-1])}"
+    print(new_file_path)
+    os.rename(actual_table_path,file_path)
+
+if __name__ == "__main__":
     directory_path = "."
     subdirectories_list = get_subdirectories_paths_with_name(directory_path)
 
@@ -65,8 +75,17 @@ if __name__ == "__main__":
         corpora_inf = value[0]
         for element in value[1]:
             result = process_files(f"{key}/{key}_request_json/{element}",f"{key}/{corpora_inf}/sud_{key}.json")
-            print(f"{key}/{key}_request_json/{element}")
-            with open(f"{key}/{key}_table_json/{element}",'w') as output:
+            real_name = element.split("_")
+            new = "_".join(real_name[1:])
+            new_name = f"table_{new}"
+            get_all_table.append(new_name)
+            with open(f"{key}/{key}_table_json/{new_name}",'w') as output:
                 output.write(str(result))
 
-        #get_all_table.append(result)
+        for element in get_all_table:
+            directory = "../static/docs/"
+            filename = element
+            actual_table_path = f"{key}/{key}_table_json/{element}"
+            print(f"directory = {directory}\n filename = {filename}\n actual_table_path = {actual_table_path}")
+            replace_file("../static/docs/","table_output_french_subj.json","french/french_table_json/table_output_french_subj.json")
+
